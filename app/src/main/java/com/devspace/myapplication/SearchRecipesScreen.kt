25727b56fd4f150d78970cf0190fb235c23532cd
Roque Buarque Junior <11612508+roquebuarquejr@users.noreply.github.com>
2024-06-27 17:57:52 +0100

    Update SearchRecipesScreen.kt

--- app/src/main/java/com/devspace/myapplication/SearchRecipesScreen.kt
+++ app/src/main/java/com/devspace/myapplication/SearchRecipesScreen.kt
@@ -12,21 +12,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -125,14 +124,10 @@ private fun SearchRecipeItem(
             }
     ) {
 
-        Card(
-            modifier = Modifier
-                .fillMaxWidth(),
-            elevation = CardDefaults.cardElevation(2.dp)
-        ) {
             AsyncImage(
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
+                    .clip(RoundedCornerShape(topEnd = 8.dp , topStart = 8.dp))
                     .fillMaxWidth()
                     .height(150.dp),
                 model = searchRecipeDto.image, contentDescription = "${searchRecipeDto.title} Image"
@@ -144,6 +139,5 @@ private fun SearchRecipeItem(
                 fontWeight = FontWeight.SemiBold,
                 text = searchRecipeDto.title
             )
-        }
     }
 }
